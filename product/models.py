73c235c9aa3be29909f0ac013f748b7a8cf05225
hello <emilavazov@gmail.com>
2021-10-31 01:26:36 +0600

    fixed appearances in lines 13 and 32

--- product/models.py
+++ product/models.py
@@ -8,13 +8,9 @@ from django.db import models
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=50, 
-                             unique=True)
+    title = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(primary_key=True)
-    parent = models.ForeignKey('self', blank=True, 
-                                null=True, 
-                                on_delete=models.CASCADE,
-                                related_name='children')
+    parent = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE, related_name='children')
 
     def __str__(self) -> str:
         if self.parent:
@@ -29,15 +25,11 @@ class Product(models.Model):
     )
 
     name = models.CharField(max_length=155)
-    status = models.CharField(choices=STATUS_CHOICES,
-                              max_length=20)
-    price = models.DecimalField(max_digits=10,
-                                decimal_places=2)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=20)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     image = models.ImageField(upload_to='prod_images')
-    category = models.ForeignKey(Category, 
-                                 on_delete=models.CASCADE,
-                                 related_name='products')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
 
 
     def __str__(self):
