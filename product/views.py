09947826964c6d7997edebfa0a87a3706b0d12b1
hello <emilavazov@gmail.com>
2021-11-01 23:36:23 +0600

    adding 2 new files and 3 modified

--- product/views.py
+++ product/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Category, Product
 
-# Create your views here.
+def home_page(request):
+    categories = Category.objects.all()
+    return render(request, 'home.html', locals())
+
+def product_list(request, slug):
+    products = Product.objects.filter(category__slug=slug)
+    # SELECT * FROM Product WHERE category.slug = slug
+    return render(request, 'product_list.html', locals())
--- shop/urls.py
+++ shop/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from product.views import home_page, product_list
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page),
+    path('<str:slug>/', product_list, name='list'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
