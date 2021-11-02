9e5dab2d5aa0b7a0e0d871c08c910c124cfdf426
hello <emilavazov@gmail.com>
2021-11-02 21:11:45 +0600

    Adding new files, and modifiction

--- product/views.py
+++ product/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .forms import CreateProductForm
 from .models import Category, Product
+
 
 def home_page(request):
     categories = Category.objects.all()
     return render(request, 'home.html', locals())
 
+
 def product_list(request, slug):
     products = Product.objects.filter(category__slug=slug)
     # SELECT * FROM Product WHERE category.slug = slug
     return render(request, 'product_list.html', locals())
+
+
+def product_detail(request, product_id):
+    # product = Product.objects.get(id=product_id)
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'detail.html', locals())
+
+
+def product_create(request):
+    if request.method == 'POST':
+        print(request.POST)
+        product_form = CreateProductForm(request.POST, request.FILES)
+        if product_form.is_valid():
+            product = product_form.save()
+            return redirect('detail', product.id)
+    else:
+        product_form = CreateProductForm()
+    return render(request, 'create_product.html', locals())
--- shop/urls.py
+++ shop/urls.py
@@ -17,14 +17,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from product.views import home_page, product_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page),
-    path('<str:slug>/', product_list, name='list'),
+    path('', include('product.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
